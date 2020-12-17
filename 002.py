4b1cc7737b8778f093f72aca4a80a53159cedcd6
Francisco Javier Lledó Fuster <51862214+javilledo@users.noreply.github.com>
2020-12-17 16:11:58 +0100

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,13 @@
+
+def fibo(index):
+    if index == 0: return 1
+    if index == 1: return 2
+    return fibo(index - 1) + fibo (index - 2)
+
+index = 0
+sum = 0
+while fibo(index) < 4000000:
+    if fibo(index) % 2 == 0: sum += fibo(index)
+    index += 1
+
+print('PROBLEM 2: %d' % (sum))
