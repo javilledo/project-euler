132d364dc477685d044da5415b82a38b020dd2d4
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2022-04-10 11:48:59 +0200

    problem 5 solved!

--- 005.py
+++ 005.py
@@ -1,7 +1,3 @@
-# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
 MCM = 11 * 13 * 7 * 5 * 16 * 17 * 9 * 19
 
 print('PROBLEM 5: %d' % (MCM)) 
