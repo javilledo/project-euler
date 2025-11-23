f8ac54319c0a117e1f98ebc021dd1d8d371b8b33
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2025-11-23 11:25:27 +0100

    feat: Solve Project Euler Problem 35 by implementing circular prime detection, correcting helper functions, and removing an unused import.

--- 035.py
+++ 035.py
@@ -1,8 +1,6 @@
 
 import itertools
 
-from matplotlib.pyplot import get
-
 def is_prime(n):
     if (n <= 1) :
         return False
@@ -14,11 +12,11 @@ def is_prime(n):
     while(i * i <= n) :
         if (n % i == 0 or n % (i + 2) == 0) :
             return False
-    i = i + 6
+        i = i + 6
     return True
 
 def get_digits(n):
-    return [int(el) for el in list(str(334))]
+    return [int(el) for el in list(str(n))]
 
 def get_permutations(n):
     res = []
@@ -30,10 +28,40 @@ def get_permutations(n):
         res.append(int(c))
     return(res)
 
+def circular_rotation(n):
+    """
+    Devuelve todas las rotaciones circulares de un número.
+    Por ejemplo: 197 -> [197, 971, 719]
+    """
+    res = []
+    s = str(n)
+    length = len(s)
+    
+    for i in range(length):
+        # Rotar: tomar desde posición i hasta el final + desde inicio hasta posición i
+        rotated = s[i:] + s[:i]
+        res.append(int(rotated))
+    
+    return res
 
 
+res = 0
 
+# Recorrer números del 1 al 1000000
+for num in range(1, 1000001):
+    # Obtener todas las rotaciones circulares
+    rotations = circular_rotation(num)
+    
+    # Verificar si todas las rotaciones son primas
+    all_prime = True
+    for rotation in rotations:
+        if not is_prime(rotation):
+            all_prime = False
+            break
+    
+    # Si todas las rotaciones son primas, incrementar res
+    if all_prime:
+        res += 1
+        print(f"{num}: {rotations} - Todas son primas")
 
-
-
-
+print('PROBLEM 35: %d' % (res))
